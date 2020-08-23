4dde97882f2a9c8ef071f8995b102d1332580741
Tony Fiore <tdfiore@gmail.com>
2020-08-22 19:54:44 -0700

    Implement BST constructor and insert

---
+++ binary_search_tree.py
@@ -0,0 +1,33 @@
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+    
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = BSTNode(value)
+        if self.root == None:
+            self.root = new_node
+            return
+        
+        compared_node = self.root
+        while compared_node != None:
+            if value > compared_node.value:
+                if compared_node.right == None:
+                    compared_node.right = new_node
+                    return
+                else:
+                    compared_node = compared_node.right
+            else:
+                if compared_node.left == None:
+                    compared_node.left = new_node
+                    return
+                else:
+                    compared_node = compared_node.left
+
+    
