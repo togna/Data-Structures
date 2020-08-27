4998f03ab1c2b916f42db8fad557628ca39d8581
Tony Fiore <tdfiore@gmail.com>
2020-08-26 21:38:18 -0700

    Implement tests for search

--- test_binary_search_tree.py
+++ test_binary_search_tree.py
@@ -51,6 +51,51 @@ class TestBST(unittest.TestCase):
         self.test_tree.insert(0)
         self.assertEqual(self.test_tree.root.left.right.value, 0)
 
+    def test_search_empty_tree(self):
+        self.assertFalse(self.test_tree.search(1))
+
+    def test_search_missing_value(self):
+        self.test_tree.insert(1)
+        self.assertFalse(self.test_tree.search(2))
+
+    def test_search_root(self):
+        self.test_tree.insert(1)
+        self.assertTrue(self.test_tree.search(1))
+
+    def test_search_right(self):
+        self.test_tree.insert(1)
+        self.test_tree.insert(2)
+        self.assertTrue(self.test_tree.search(2))
+
+    def test_search_left(self):
+        self.test_tree.insert(1)
+        self.test_tree.insert(0)
+        self.assertTrue(self.test_tree.search(0))
+
+    def test_search_right_right(self):
+        self.test_tree.insert(1)
+        self.test_tree.insert(2)
+        self.test_tree.insert(3)
+        self.assertTrue(self.test_tree.search(3))
+
+    def test_search_right_left(self):
+        self.test_tree.insert(1)
+        self.test_tree.insert(3)
+        self.test_tree.insert(2)
+        self.assertTrue(self.test_tree.search(2))
+
+    def test_search_left_left(self):
+        self.test_tree.insert(1)
+        self.test_tree.insert(0)
+        self.test_tree.insert(-1)
+        self.assertTrue(self.test_tree.search(-1))
+
+    def test_search_left_right(self):
+        self.test_tree.insert(1)
+        self.test_tree.insert(-1)
+        self.test_tree.insert(0)
+        self.assertTrue(self.test_tree.search(0))
+
 
 if __name__ == "__main__":
     unittest.main()
